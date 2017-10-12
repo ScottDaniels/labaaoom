952de31d575b042f88702d09936716a8ffb1872f
Scott Daniels <escottdaniels@gmail.com>
2017-10-12 00:35:22 -0400

    Add evaluation setup activity

---
+++ app/src/main/java/com/rocklizard/labaaoom/Eval_setup.java
@@ -0,0 +1,13 @@
+package com.rocklizard.labaaoom;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Eval_setup extends Activity {
+
+	@Override
+	protected void onCreate( Bundle savedInstanceState ) {
+		super.onCreate( savedInstanceState );
+		setContentView( R.layout.activity_eval_setup );
+	}
+}
