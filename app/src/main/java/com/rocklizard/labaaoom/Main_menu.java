ab0f4bab65b9bced32d1ec103875446cd78b52ed
Scott Daniels <escottdaniels@gmail.com>
2017-10-12 23:28:59 -0400

    Invoke eval setup

--- app/src/main/java/com/rocklizard/labaaoom/Main_menu.java
+++ app/src/main/java/com/rocklizard/labaaoom/Main_menu.java
@@ -50,7 +50,18 @@ public class Main_menu extends Activity {
     }
 
     public void Go_evaluate( View v ) {
-		Toast.makeText(this, "Starting Evaluation", Toast.LENGTH_LONG).show();
+		Class targetc;
+		Intent target;
+		Bundle bun;
+
+		bun = new Bundle();
+		bun.putString( "target_name", "eval" );			// pass what the selection is to drive
+
+		targetc = Student_list.class;
+		target = new Intent( this, targetc );
+		target.putExtras( bun );
+		startActivity( target );
+		//Toast.makeText(this, "Starting Evaluation", Toast.LENGTH_LONG).show();
     }
 
 	public void Go_view_student( View v ) {
