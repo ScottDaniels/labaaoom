4184ddf578364e65445a570d7e58fe0d9d597285
Scott Daniels <escottdaniels@gmail.com>
2017-10-12 23:14:50 -0400

    Add funcitons to eval setup

--- app/src/main/java/com/rocklizard/labaaoom/Eval_setup.java
+++ app/src/main/java/com/rocklizard/labaaoom/Eval_setup.java
@@ -2,6 +2,8 @@ package com.rocklizard.labaaoom;
 
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Toast;
 
 public class Eval_setup extends Activity {
 
@@ -9,5 +11,42 @@ public class Eval_setup extends Activity {
 	protected void onCreate( Bundle savedInstanceState ) {
 		super.onCreate( savedInstanceState );
 		setContentView( R.layout.activity_eval_setup );
+	}
+
+	/*
+		Called when start random button pressed. Sets up and invokes
+		an evaluation on random word groups.
+	*/
+	public void Start_rand_eval( View v ) {
+		Toast.makeText(this, "start random eval button clicked", Toast.LENGTH_LONG).show();
+	}
+
+	/*
+		Called when start random button pressed. Sets up and invokes
+		an evaluation on random word groups.
+	*/
+	public void Start_sent_eval( View v ) {
+		Toast.makeText(this, "start sentence eval button clicked", Toast.LENGTH_LONG).show();
+	}
+
+	/*
+		Invoked when a button in the background adjustment group is clicked
+	*/
+	public void Adjust_bg( View v ) {
+		Toast.makeText(this, "radio button callback driven for bg", Toast.LENGTH_LONG).show();
+	}
+
+	/*
+		Invoked when a button in the style adjustment group is clicked
+	*/
+	public void Adjust_style( View v ) {
+		Toast.makeText(this, "radio button callback driven for style", Toast.LENGTH_LONG).show();
+	}
+
+	/*
+		Invoked when a button in the size adjustment group is clicked
+	*/
+	public void Adjust_size( View v ) {
+		Toast.makeText(this, "radio button callback driven for size", Toast.LENGTH_LONG).show();
 	}
 }
