a6502b24c3378762a2eaaacd31531277c73abc03
Scott Daniels <escottdaniels@gmail.com>
2017-10-14 11:30:46 -0400

    Add settings to student class

--- app/src/main/java/com/rocklizard/labaaoom/Student.java
+++ app/src/main/java/com/rocklizard/labaaoom/Student.java
@@ -8,38 +8,44 @@ package com.rocklizard.labaaoom;
 */
 
 public class Student {
-	static final boolean NORMAL = true;
-	static final boolean LOWCONTRAST = false;
+	static final boolean SENTENCES = true;
+	static final boolean RANDOM = false;
 
 	private String name;
 	private String section;			// section that the kid belongs to (K1 First1 etc)
-	private Eval_set norm_evals;    // list of all normal evaluations that have been done
-	private Eval_set lc_evals;        // list of all low contrast evaluations that have been done
+	private Eval_set sentence_evals;    // list of all evaluations using sentences
+	private Eval_set rand_evals;        // list of all evaluations using random words
+	private Settings settings;			// evaluation settings last used
 
 	public Student( String name, String sect ) {
 		this.name = name;
 		this.section = sect;
-		norm_evals = new Eval_set( );
-		lc_evals = new Eval_set( );
+		sentence_evals = new Eval_set( );
+		rand_evals = new Eval_set( );
+		settings = new Settings();			// get default settings
 	}
 
 	// ---- static methods ----------------
 	/*
 		Given a datacache entry set (array of strings) build a student object.
+		The settings for a student are also cached in the same list, so as a part of this build
+		we generate a settings object which is referenced by the student.
 		Strings are of the type:
 			name:<student name tokens>
 			sect:<section name>
-			ne:<normal evaluation set>
-			le:<low contrast evaluation set>
+			sen:<sentence evaluation set>
+			rand:<random word evaluation set>
 	*/
 	public Student( String[] dc_entry ) {
 		int i;
 		String[] tokens;        // split input data
 
-		norm_evals = new Eval_set( );
-		lc_evals = new Eval_set( );
+		sentence_evals = new Eval_set( );
+		rand_evals = new Eval_set( );
 
 		if( dc_entry == null ) {			// shouldn't happen but parinoia isn't always bad
+				add_name( "information missing" );
+				settings = new Settings();
 				return;
 		}
 
@@ -55,17 +61,25 @@ public class Student {
 					add_sect( tokens[1] );
 					break;
 
-				case "ne":
+				case "se":
 					Add_evaluation( tokens[ 1 ], true );
 					break;
 
-				case "le":
+				case "re":
 					Add_evaluation( tokens[ 1 ], false );
+					break;
+
+				case "sets":			// pick up settings and build object
+					settings = new Settings( tokens[1] );
 					break;
 
 				default:
 					break;
 			}
+		}
+
+		if( settings == null ) {
+			settings = new Settings( ); 		// ensure there are defaults if not in the dc
 		}
 	}
 
@@ -82,20 +96,21 @@ public class Student {
 	}
 
 	/*
-		Add an evaluation to one of the lists. List is determined by the norm bool
-		flag; if true the normal list is assumed, else the low contrast list is assumed.
+		Add an evaluation to one of the lists. List is determined by the sentence bool
+		flag; if true the sentence list is assumed, else the random word evaluation
+		list is assumed.
 	*/
-	public void Add_evaluation( String csl, boolean norm ) {
-		if( norm ) {
-			norm_evals.Add_entry( csl );            // add the entry
+	public void Add_evaluation( String csl, boolean sentence ) {
+		if( sentence ) {
+			sentence_evals.Add_entry( csl );            // add the entry
 		} else {
-			lc_evals.Add_entry( csl );
+			rand_evals.Add_entry( csl );
 		}
 	}
 
 	/*
 		Generate a datacache entry from this instance. The datacache entry is an array
-		of strings with name, normal evaluation and low contrast evaluation information.
+		of strings with name, sentence  and random word evaluation information.
 	*/
 	public String[] GenDcEntry() {
 		String[] strs;				// set of strings to return
@@ -103,31 +118,33 @@ public class Student {
 		int i = 0;
 		int j;
 
-		if( norm_evals != null ){
-			i += norm_evals.GetSize();
+		if( sentence_evals != null ){
+			i += sentence_evals.GetSize();
 		}
-		if( lc_evals != null ){
-			i += lc_evals.GetSize();
+		if( rand_evals != null ){
+			i += rand_evals.GetSize();
 		}
 
 		strs = new String[i+1];
 		i = 0;
 		strs[i++] = "name:" + name;
 		strs[i++] = "sect:" + section;
+		strs[i++] = "sets:" + settings.ToString();
 
-		if( norm_evals != null ) {
-			evals = norm_evals.ToStrings();
+		if( sentence_evals != null ) {
+			evals = sentence_evals.ToStrings();
 			if( evals != null ) {
 				for( j = 0; j < evals.length; j++ ) {
-					strs[ i++ ] = "ne:" + evals[ j ];
+					strs[ i++ ] = "se:" + evals[ j ];
 				}
 			}
 		}
-		if( lc_evals != null ) {
-			evals = lc_evals.ToStrings();
+
+		if( rand_evals != null ) {
+			evals = rand_evals.ToStrings();
 			if( evals != null ) {
 				for( j = 0; j < evals.length; j++ ) {
-					strs[ i++ ] = "le:" + evals[ j ];
+					strs[ i++ ] = "re:" + evals[ j ];
 				}
 			}
 		}
@@ -136,46 +153,46 @@ public class Student {
 	}
 
 	/*
-		Generate a set of graph data for either the normal (true) or low contrast evaluations.
+		Generate a set of graph data for either the sentence (true) or random word evaluations.
 	*/
-	public int[] GetData( boolean norm ) {
+	public int[] GetData( boolean sentence ) {
 
-		if( norm ) {
-			return norm_evals.GetData();
+		if( sentence ) {
+			return sentence_evals.GetData();
 		}
 
-		return lc_evals.GetData();
+		return rand_evals.GetData();
 	}
 
 	/*
 		Find the last eval and return it or nil if no evals exist.
 	*/
 	public Evaluation GetLastEval() {
-		Evaluation evn = null;		// last normal evaluation
-		Evaluation evl = null;		// last low contrast evaluation
+		Evaluation evs = null;		// last senence based evaluation
+		Evaluation evr = null;		// last random word evaluation
 
 
-		if( norm_evals != null ){
-			evn = norm_evals.GetLast();
+		if( sentence_evals != null ){
+			evs = sentence_evals.GetLast();
 		}
 
-		if( lc_evals != null ) {
-			evl = lc_evals.GetLast();
+		if( rand_evals != null ) {
+			evr = rand_evals.GetLast();
 		}
 
-		if( evl == null ) {				// if either is nil, just return the other
-			return evn;
+		if( evr == null ) {				// if either is nil, just return the other
+			return evs;
 		} else {
-			if( evn == null ) {
-				return evl;
+			if( evs == null ) {
+				return evr;
 			}
 		}
 
-		if( evl.GetTimestamp() > evn.GetTimestamp() ) {			// return the larger
-			return evl;
+		if( evr.GetTimestamp() > evs.GetTimestamp() ) {			// return the larger
+			return evr;
 		}
 
-		return evn;
+		return evs;
 	}
 
 	/*
@@ -193,19 +210,19 @@ public class Student {
 	}
 
 	/*
-		Return the min/max/ave/instances counts for either the normal (true) eval set
-		or the low contrast (false) eval set.
+		Return the min/max/ave/instances counts for either the sentence based evaluations (true) 
+		eval set or the random word (false) eval set.
 	*/
-	public double[] GetMMAI( boolean norm ) {
-		if( norm ) {
-			if( norm_evals != null ) {
-				return norm_evals.GetMMAI();
+	public double[] GetMMAI( boolean sentences ) {
+		if( sentences ) {
+			if( sentence_evals != null ) {
+				return sentence_evals.GetMMAI();
 			}
 
 			return null;
 		} else {
-			if( lc_evals != null ) {
-				return lc_evals.GetMMAI();
+			if( rand_evals != null ) {
+				return rand_evals.GetMMAI();
 			}
 
 			return null;
