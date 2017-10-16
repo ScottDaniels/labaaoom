672995dc254103eb5960152c652a8ef896ca2f20
Scott Daniels <escottdaniels@gmail.com>
2017-10-15 22:40:57 -0400

    Add datacache entry support to evaluation

--- app/src/main/java/com/rocklizard/labaaoom/Evaluation.java
+++ app/src/main/java/com/rocklizard/labaaoom/Evaluation.java
@@ -5,6 +5,8 @@ package com.rocklizard.labaaoom;
 	Abstract:	Manages one evaluation
 	Date:		17 September 2017
 	Author:		E. Scott Daniels edaniels7@gatech.edu  cs6460 Fall 2017
+
+	Mods:		15 Oct 2017 - Add datacache stash/load functions.
 */
 
 
@@ -15,7 +17,7 @@ public class Evaluation {
 	private long	timestamp;		// timestamp cooresponding to date
 	private double	wpm;			// words per minute of the eval
 	private String	eval_id;		// reading set id (e.g. K-1)
-	private String	eval_type;		// norm/lc or maybe somthing else
+	private String	eval_type;		// sentence/random etc.
 	private boolean accepted;		// instructor has accepted the eval as valid
 
 	/*
@@ -61,6 +63,56 @@ public class Evaluation {
 	}
 
 	/*
+		Generate an evaluation instance based on a datacache entry of strings.
+	*/
+	public Evaluation( String[] entry ) {
+		int i;
+		String[] tokens;
+
+		for( i = 0; i < entry.length; i++ ) {
+			tokens = entry[i].split( ":" );
+			switch( tokens[0] ) {
+				case "timestamp":
+					timestamp = Long.parseLong( tokens[1] );
+					break;
+
+				case "wpm":
+					wpm = Double.parseDouble( tokens[1] );
+					break;
+
+				case "id":
+					eval_id = tokens[1];
+					break;
+
+				case "type":
+					eval_type = tokens[1];
+					break;
+
+				case "accepted":
+					accepted = tokens[1].equals( "true" );
+					break;
+			}
+		}
+	}
+
+	/*
+		Generate a datacache entry that is used when saving a pending evaluation.
+		Returns an array of strings that will be put into the cache.
+	*/
+	public String[] GenDcEntry( ) {
+		String[] entry;
+
+		entry = new String[5];
+		entry[0] = "timestamp:" + Long.toString( timestamp );
+		entry[1] = "wpm:" + Double.toString( wpm );
+		entry[2] = "id:" + eval_id;
+		entry[3] = "type:" + eval_type;
+		entry[4] = "accepted:" + (accepted ? "true" : "false");
+
+		return entry;
+	}
+
+	/*
 		Fetch up the evaluation's word per minute score.
 	*/
 	public double GetWpm( ) {
@@ -87,7 +139,7 @@ public class Evaluation {
 
 	/*
 		Build a comma separated string with our event info; order out must match
-		the order expected from the string onstructor: set, type, wpm, timestamp, accepted.
+		the order expected from the string constructor: set, type, wpm, timestamp, accepted.
 	*/
 	public String ToString( ) {
 		String s;
